c6e11ac92575982fbae8d763d2d30e8a65e6c1f9
Hackbright Student <info@hackbrightacademy.com>
2014-06-16 18:01:15 -0700

    Halfway thru expansions

--- hackbright_app.py
+++ hackbright_app.py
@@ -11,6 +11,30 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT INTO Students VALUES (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+def project_by_title(project_title):
+    query = """SELECT (?), description, max_grade FROM Projects"""
+    DB.execute(query, (project_title,))
+
+    row = DB.fetchone()
+    print """\
+Project: %s
+Project Description: %s
+Max Grade: %d"""%(row[0], row[1], row[2])
+
+def add_project(project_name, project_description, max_grade):
+    query = """INSERT INTO Projects VALUES (?, ?, ?)"""
+    DB.execute(query, (project_name, project_description, max_grade))
+
+    CONN.commit()
+    print "Successfully added project %s" % (project_name)
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
@@ -20,8 +44,9 @@ def main():
     connect_to_db()
     command = None
     while command != "quit":
+
         input_string = raw_input("HBA Database> ")
-        tokens = input_string.split()
+        tokens = input_string.split(",")
         command = tokens[0]
         args = tokens[1:]
 
@@ -29,6 +54,10 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "project":
+            project_by_title(*args)
+        elif command == "new_project":
+            add_project(*args)
 
     CONN.close()
 
